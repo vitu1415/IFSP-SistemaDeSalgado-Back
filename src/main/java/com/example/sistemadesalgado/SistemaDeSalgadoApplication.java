7140cc05444dd30d9c8afeec2f6d036b4d3588a2
vitu1415 <vitoralves0801@gmail.com>
2026-06-06 18:21:54 -0300

    first commit

---
+++ src/main/java/com/example/sistemadesalgado/SistemaDeSalgadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemadesalgado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeSalgadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeSalgadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemadesalgado/SistemaDeSalgadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemadesalgado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeSalgadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
